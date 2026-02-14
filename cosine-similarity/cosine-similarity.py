5098ed466bee4c474267aaf0911d7afdb978606a
Nitiz Khanal <67851367+StarAtNyte@users.noreply.github.com>
2026-02-15 01:05:30 +0545

    Update solution for cosine-similarity

--- cosine-similarity/cosine-similarity.py
+++ cosine-similarity/cosine-similarity.py
@@ -6,15 +6,7 @@ def cosine_similarity(a, b):
     Compute cosine similarity between two 1D NumPy arrays.
     Returns: float in [-1, 1]
     """
-    numerator = np.dot(a,b)
-    denominator =  norm(a) * norm(b)
+    denominator  = (np.linalg.norm(a) * np.linalg.norm(b))
     if denominator == 0:
       return 0.0
-    return numerator/denominator
-    pass
-
-def norm(vector):
-  sum = 0
-  for item in vector:
-    sum += item **2
-  return math.sqrt(sum)
+    return (np.dot(a, b)) / denominator
